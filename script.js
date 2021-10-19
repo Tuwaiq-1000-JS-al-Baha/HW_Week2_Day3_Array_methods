b5ae3696c6bbbe38df2fb77e64211554a2f27dd7
Najwaageel <Najwa.Ageel.abt.js@tuwaiq.edu.sa>
2021-10-19 22:46:25 +0300

    HW week2 day 3

---
+++ script.js
@@ -0,0 +1,22 @@
+const friends = [ " Melissa " , " Marc " , " Andrew " , " Nick " ] ;
+friends.forEach ( function ( friend ) {
+console.log( " I have a friend named " + friend.toLowerCase () ) ; } )
+
+
+
+    const numbers = [ 1 , 5 , 8 ]
+    const numbersSquared = numbers.map ( num => num * num )
+    console.log(numbersSquared)
+
+
+
+    const instructors = [ " Alex " , " Daniel " , " Morgan " , " Ben " , " Micah " , " Jims " ] ;
+    const instructor2 = instructors.map ( function ( instructor )
+     { 
+         return instructor + " is awesome " 
+        })
+        console.log( instructor2 )
+
+
+
+    
