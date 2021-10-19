726ac02f731957f87a6443f325645b25a32e9a21
Eyad5501 <Eyad.Ahmed.abt.js@tuwaiq.edu.sa>
2021-10-20 02:12:03 +0300

    Create scrpit3.js

---
+++ scrpit3.js
@@ -0,0 +1,11 @@
+//map==array
+const instructors = ["Alex", "Daniel", "Morgan", "Ben", "Micah", "Jims"];
+const instr=instructors.map(function(instructors){
+    return instructors;
+})
+console.log("instructors",instr)
+//forEach==loop
+const instructors1 = ["Alex", "Daniel", "Morgan", "Ben", "Micah", "Jims"];
+instructors.forEach(function(instructors){
+    console.log("instructors:",instructors)
+})
