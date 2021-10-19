a277efac23861eee3c6d3e4f3725c6664670fa79
Ghadah-Mohammed <92169172+Ghadah-Mohammed@users.noreply.github.com>
2021-10-20 01:17:16 +0300

    HW.w2d4

---
+++ script.js
@@ -0,0 +1,25 @@
+const frinds=["ghada", "malak" ,"ghaida"];
+
+frinds.forEach(function(frind){
+    console.log("I have a friend named " + frind);
+});
+
+
+// -----------------------------------------------------
+const numbers = [1, 5, 8];
+const numbersTimesTwo = numbers.map(function(num) {
+  return num * 2;
+  
+});
+const timesTwo = numbers.map(num => num * 2);
+console.log(numbers)
+
+
+//------------------------------------------------------------
+
+
+const instructors = ["Alex", "Daniel", "Morgan", "Ben", "Micah", "Jims"];
+const tt=instructors.map(function(instructors){
+    console.logt(tt+"is awesome")  
+
+}
