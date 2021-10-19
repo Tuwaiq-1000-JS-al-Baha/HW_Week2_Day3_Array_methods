e5991082d0fad74eb829689bb2d61cb73e054202
Eyad5501 <Eyad.Ahmed.abt.js@tuwaiq.edu.sa>
2021-10-20 02:11:54 +0300

    Create scrpit1.js

---
+++ scrpit1.js
@@ -0,0 +1,12 @@
+//map==array
+const numbers = [1, 2, 3];
+const numbersTimesTwo = numbers.map(function(num) {
+  return num * 2;
+})
+console.log("numbersTimesTwo",numbersTimesTwo)
+//for.Each==loop
+const numbers1= [1, 2, 3]
+   numbers1.forEach(function(numbers1){
+    numbers1=numbers1 * 2
+    console.log("numbersTimesTwo:", numbers1) ;
+})
