4300492a8021a9ac1e4bc08e7cbdd6de826132e2
Eyad5501 <Eyad.Ahmed.abt.js@tuwaiq.edu.sa>
2021-10-20 02:12:06 +0300

    Create scrpit4.js

---
+++ scrpit4.js
@@ -0,0 +1,8 @@
+//map==array
+const nameawesome=["Alex is awesome", "Daniel is awesome", "Morgan is awesome", "Ben is awesome", "Micah is awesome", "Jims is awesome"];
+const awesome1 = nameawesome.map(nameawesome => console.log(nameawesome))
+
+
+//forEach==loop
+const nameawesome1=["Alex is awesome", "Daniel is awesome", "Morgan is awesome", "Ben is awesome", "Micah is awesome", "Jims is awesome"];
+nameawesome.forEach(nameawesome1=>console.log(nameawesome))
