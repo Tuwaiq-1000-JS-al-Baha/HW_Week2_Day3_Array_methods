48d98a76a877794c8173ba7556bed6fa8e82f49c
X702-2025 <mohmmad201048@gmail.com>
2021-10-19 19:06:25 +0300

    arrayMethodsDone

---
+++ script.js
@@ -0,0 +1,40 @@
+//                     Question 1\\
+
+// const friends = ["Melissa", "Marc", "Andrew", "Nick"];
+
+// friends.forEach(function(friend) {
+//   console.log("I have a friend named " + friend.toLowerCase());
+// });
+
+// const numbers = [1, 2, 3];
+// const numbersTimesTwo = numbers.map(function(num) {
+//   return num * 2;
+// });
+// console.log(numbersTimesTwo)
+
+// const timesTwo = numbers.map(num => num * 2);
+
+// console.log(timesTwo)
+
+
+//                     Question 2\\
+
+// const numbers = [1, 5, 8]
+// const numbersSquared = numbers.map(function(num) {
+//     return num * num;
+//   });
+  
+//   console.log(numbersSquared)
+
+//                 //Question 2 BONUS\\
+
+// const square = numbers.map(num => num * num)
+// console.log(square)                
+
+//                     Question 3\\
+
+// const instructors = ["Alex", "Daniel", "Morgan", "Ben", "Micah", "Jims"];
+// const instr = instructors.map(function(instructor) {
+// return instructor + " is awesome"
+// })
+// console.log(instr)
