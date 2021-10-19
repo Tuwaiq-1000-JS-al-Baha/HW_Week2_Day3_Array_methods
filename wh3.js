052c5150d149f1391275de884cfadb67593a11fd
Magdi7021 <92311360+Magdi7021@users.noreply.github.com>
2021-10-19 20:09:20 +0300

    wh3.js

---
+++ wh3.js
@@ -0,0 +1,21 @@
+const friends = ["magdi", "eyad", "badr", "ali"];
+
+friends.forEach(function(friend) {
+  console.log("I have a friend named " + friend.toLowerCase());
+});
+
+const numbers = [1 , 5 , 8]; {
+    const numbers= numbers.map(function(numbers) {
+return numbers*2;
+const timesTwo = numbers.map(numbers => numbers * 2);
+console.log("numbers")
+});
+
+console.log(result) 
+
+const instructors = ["Alex", "Daniel", "Morgan", "Ben", "Micah", "Jims"]; 
+const instr =instructors.map(function(instructor) 
+
+{ return instructor + "is awesome"})
+
+console.log(instr)
